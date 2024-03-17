bf77464dad516c9db6a3b250bf9778a31d651d0c
Cale Kochenour <calekochenour@gmail.com>
2024-03-17 11:29:52 -0600

    Cleanup

--- tests/test_celsius_to_fahrenheit.py
+++ tests/test_celsius_to_fahrenheit.py
@@ -1,4 +1,4 @@
-from pytest import approx
+import pytest
 
 from firstpackage.temperature import celsius_to_fahrenheit
 
@@ -8,10 +8,10 @@ def test_celsius_to_fahrenheit():
     Test the celsius_to_fahrenheit function.
     """
     # Test with freezing point of water
-    assert approx(celsius_to_fahrenheit(0), abs=0.01) == 32.0
+    assert pytest.approx(celsius_to_fahrenheit(0), abs=0.01) == 32.0
 
     # Test with boiling point of water
-    assert approx(celsius_to_fahrenheit(100), abs=0.01) == 212.0
+    assert pytest.approx(celsius_to_fahrenheit(100), abs=0.01) == 212.0
 
     # Test with a negative temperature
-    assert approx(celsius_to_fahrenheit(-40), abs=0.01) == -40.0
+    assert pytest.approx(celsius_to_fahrenheit(-40), abs=0.01) == -40.0
