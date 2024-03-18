e31fcbd381b6b9a6ffa30bbb33ade98d45e0812a
Cale Kochenour <calekochenour@gmail.com>
2024-03-18 00:22:47 -0600

    Cleanup

--- noxfile.py
+++ noxfile.py
@@ -9,9 +9,8 @@ import nox
 ###############################################################################
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def test(session):
-
     # install - needed if there are test dependencies
     #  (should already be installed for my config)
     # session.install(".[tests]")
@@ -31,18 +30,15 @@ def test(session):
 ###############################################################################
 
 
-@nox.session(
-    venv_backend='conda', python=["3.8", "3.9", "3.10", "3.11", "3.12"]
-)
+@nox.session(venv_backend="conda", python=["3.9", "3.10", "3.11", "3.12"])
 def test_conda(session):
     """Nox function that installs dev requirements and runs
     tests on Python 3.8 through 3.12
     """
-
     # Install dev requirements
     # session.conda_install(".[tests]", channel='conda-forge')
     session.install(".")
-    session.install('pytest')
+    session.install("pytest")
     # session.install('pytest-cov', '--no-deps')
     # Run tests using any parameters that you need
     session.run("pytest")
